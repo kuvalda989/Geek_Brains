c0d8251822d27117d78067e5ee672094c636a56d
Dmitry Podlipaev <kuvalda989@mail.ru>
2018-12-18 10:36:17 +0400

    4 exercise

--- homework_1.py
+++ homework_1.py
@@ -1,3 +1,5 @@
+#First exercise 
+
 words_1 = ['разработка', 'сокет', 'декоратор']
 
 for word in words_1:
@@ -11,5 +13,33 @@ for word in words_1:
 	print('================Decoding bytes to str')
 	word_utf = byte_word.decode('utf-8')
 	print(word_utf)
+
+# Second exercise
+words_2 = [b'class', b'function', b'method']
+
+
+for word in words_2:
+	print('==================Primary word')
+	print(word)
+	print(type(word))
+	print(len(word))
+
+#Third exercise 
+# words_3 = [b'attribute', b'класс', b'function', b'type'] <===  b'класс' невозможно записать
+
+
+#Fourth exercise
+
+words_4 = ['attribute', 'класс', 'protocol', 'standart']
+
+for word in words_4:
+	print('\n\n==================Primary word')
+	print(word)
+	print('================Encoding str to bytes')
+	word_byte = word.encode()
+	print(word_byte)
+	print('================Decoding bytes to str')
+	word = word_byte.decode()
+	print(word)
 
 
