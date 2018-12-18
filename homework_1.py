3d6681edbc9e5b82ca93a0168d8b8c2364cbff0e
Dmitry Podlipaev <kuvalda989@mail.ru>
2018-12-18 09:41:16 +0400

    First Task

--- homework_1.py
+++ homework_1.py
@@ -1,1 +1,15 @@
+words_1 = ['разработка', 'сокет', 'декоратор']
+
+for word in words_1:
+	print('==================Primary word')
+	print(word)
+	print(type(word))
+	print('================Encoding str to bytes')
+	byte_word = bytes(word, 'utf-8')
+	print(byte_word)
+	print(type(byte_word))
+	print('================Decoding bytes to str')
+	word_utf = byte_word.decode('utf-8')
+	print(word_utf)
+
 
