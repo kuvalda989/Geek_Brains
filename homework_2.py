2c72a58c6a42ee69a0521ad4daecc9781671982e
kuvalda989 <kuvalda989@mail.ru>
2018-12-23 16:31:16 +0400

    for tests

--- homework_2.py
+++ homework_2.py
@@ -1,1 +1,27 @@
+import csv
+import json
 
+
+
+# with open('for_homework/info_1.txt', encoding='cp1251') as f_n:
+# 	f_n_reader = csv.reader(f_n)
+# 	for row in f_n_reader:
+# 		print(row)
+
+
+# with open('for_homework/info_1.txt', encoding='cp1251') as f_n:
+# 	f_n_reader = csv.reader(f_n)
+# 	f_n_readers = next(f_n_reader)
+# 	print('Headers: ', f_n_readers)
+# 	for row in f_n_reader:
+# 		print(row)
+
+
+with open('for_homework/info_1.txt') as f_n:
+	f_n_content = csv.reader(f_n, delimiter=':')
+	# obj = json.loads(f_n_content)
+	f_n_write = []
+	for line in f_n_content:
+		f_n_write += line
+	print(f_n_write)
+	# obj = json.loads(f_n_write)
